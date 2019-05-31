2cb3dae92755b375a64134dffa45f631283a215e
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-05-31 19:24:34 -0300

    "Exercício: Calcular a diferença entre datas" updated

--- ex_diferencadatas/index.php
+++ ex_diferencadatas/index.php
@@ -1,7 +1,7 @@
 <?php 
 	require 'Tempo.class.php';
 
-	$t = '2019-05-31 08:00:00';
+	$t = '2019-05-30 18:59:30';
 	$data = date('d/m/Y H:i:s', strtotime($t));
 
 	echo '<p>'.$data.'<br>foi há '.Tempo::diferenca($t).' atrás.</p>';
