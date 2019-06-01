ea7bc15e221fc11b4b37bae0f60144e6a74125c4
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-06-01 19:58:46 -0300

    "Site de Classificados" updated (6/11)

--- classificados/classes/anuncios.class.php
+++ classificados/classes/anuncios.class.php
@@ -5,7 +5,7 @@
 
 			$array = array();
 			$sql = "SELECT *, 
-			(select anuncios_imagens.url from anuncios_imagens where anuncios_imagens.id_anuncio = anuncio.id limit 1) 
+			(select anuncios_imagens.url from anuncios_imagens where anuncios_imagens.id_anuncio = anuncios.id limit 1) 
 			as url FROM anuncios WHERE id_usuario = :id_usuario";
 			$sql = $pdo -> prepare($sql);
 			$sql -> bindValue(":id_usuario", $_SESSION['cLogin']);
@@ -16,5 +16,26 @@
 			}
 
 			return $array;
+		}
+
+		public function addAnuncio($titulo, $categoria, $valor, $descricao, $estado) {
+			global $pdo;
+
+			$sql = "INSERT INTO anuncios SET 
+			titulo = :titulo, id_usuario = :id_usuario, id_categoria = :id_categoria, descricao = :descricao, valor = :valor, estado = :estado";
+			$sql = $pdo -> prepare($sql);
+			$sql -> bindValue(":id_usuario", $_SESSION['cLogin']);
+			$sql -> bindValue(":id_categoria", $categoria);
+			$sql -> bindValue(":titulo", $titulo);
+			$sql -> bindValue(":descricao", $descricao);
+			$sql -> bindValue(":valor", $valor);
+			$sql -> bindValue(":estado", $estado);
+			$sql -> execute();
+
+			if ($sql -> rowCount() > 0) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 	}
