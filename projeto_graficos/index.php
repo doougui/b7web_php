4294c5a958c97b8de9f86e885085d50ac98924fa
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-07-05 16:02:10 -0300

    "Projeto: Criação de Gráficos" added

---
+++ projeto_graficos/index.php
@@ -0,0 +1,45 @@
+<?php 
+	// Documentação completa e exemplos (samples): https://www.chartjs.org/.
+
+	$vendas = array(10, 20, 30, 40, 20);
+	$custos = array(8, 15, 37, 98, 35);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto: Criação de Gráficos</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<div style="width: 500px;">
+		<canvas id="grafico"></canvas>
+	</div>
+
+	<script src="Chart.min.js"></script>
+	<script>
+		window.onload = function() {
+			var contexto = document.getElementById('grafico').getContext('2d');
+			var grafico = new Chart(contexto, {
+				type: 'line',
+				data: {
+					labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+					datasets: [{
+						label: 'Vendas',
+						backgroundColor: '#2f3542',
+						borderColor: '#5352ed',
+						data: [<?= implode(',', $vendas) ?>],
+						fill: false
+					}, {
+						label: 'Custos',
+						backgroundColor: '#ff4757',
+						borderColor: '#ff6b81',
+						data: [<?= implode(',', $custos) ?>],
+						fill: false
+					}]
+				}
+			});
+		};
+	</script>
+</body>
+</html>
