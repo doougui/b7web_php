39d2134a154b020ebdf9deb117ab7bbc1e11684b
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-09-15 21:07:26 -0300

    "Testes Incompletos" lesson added

--- Aulas/modulo19_testes/phpunit/classes/Usuario.php
+++ Aulas/modulo19_testes/phpunit/classes/Usuario.php
@@ -2,8 +2,18 @@
 	class Usuario {
 		private $nome;
 		private $sobrenome;
+		private $idade;
 
     public function setNome($nome) { $this -> nome = $nome; }
     public function setSobrenome($sobrenome) { $this -> sobrenome = $sobrenome; }
+    public function setIdade($idade) { $this -> idade = $idade; } 
     public function getNomeCompleto() { return $this -> nome.' '.$this -> sobrenome; }
+
+    public function getIdade($str = false) { 
+    	if ($str === true) {
+    		return $this -> idade.' anos';
+    	} else {
+    		return $this -> idade; 
+    	}
+    }
   }
--- Aulas/modulo19_testes/phpunit/tests/UsuarioTest.php
+++ Aulas/modulo19_testes/phpunit/tests/UsuarioTest.php
@@ -3,11 +3,35 @@
 
 	class UsuarioTest extends TestCase {
 		public function testExpectNomeCompleto() {
-			$this -> expectOutputString('Douglas Pinheiro Goulart');
+			$this -> expectOutputString('Douglas Goulart');
 
 			$usuario = new Usuario();
 			$usuario -> setNome('Douglas');
 			$usuario -> setSobrenome('Goulart');
 			echo $usuario -> getNomeCompleto();
+		}
+
+		public function testIdade() {
+			// $usuario = new Usuario();
+			// $usuario -> setIdade(90);
+
+			// $this -> assertEquals(
+			// 	90,
+			// 	$usuario -> getIdade()
+			// );
+			
+			$this -> markTestIncomplete('Falta implementar o set e get de idade');
+		}
+
+		public function testIdadeString() {
+			$usuario = new Usuario();
+			$usuario -> setIdade(90);
+
+			$this -> assertEquals(
+				'90 anos',
+				$usuario -> getIdade(true)
+			);
+
+			// $this -> markTestIncomplete('Falta criar o método na classe');
 		}
 	}
